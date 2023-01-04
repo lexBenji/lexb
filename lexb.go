3bbec0f504eb50021a8cd006eba63f4f3ff46bff
lexBenji <discordo12341@outlook.com>
2023-01-04 12:46:47 +0700

    lexb

---
+++ lexb.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"bufio"
+)
+
+func Check(e error) {
+	if e != nil {
+		panic(e)
+	}
+
+	return
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: lexb file.lexb")
+		os.Exit(1)
+	}
+
+	if !strings.HasSuffix(os.Args[1],".lexb") {
+		fmt.Println("usage: lexb file.lexb")
+		os.Exit(1)
+	}
+
+	fl,er := os.OpenFile(os.Args[1],os.O_RDONLY,0644)
+
+	Check(er)
+
+	scanner := bufio.NewScanner(fl)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if txt == "print:" {
+			txt = ""
+		} else if txt == "endl" {
+			fmt.Println()
+		} else {
+			fmt.Print(txt+" ")
+		}
+	}
+	fmt.Println()
+
+	defer fl.Close()
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Usage
+`lexb hello.lexb`
+
+there is
+```
+print: hello world endl
+print: very cool
+```
+in `hello.lexb`<br>
+`endl` is not needed at the end of the file
